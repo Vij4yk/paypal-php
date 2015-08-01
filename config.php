9e57176a3f64385a74e2f94d90a346a270ea6381
Rohit Kumar <rohit0kumar@hotmail.com>
2015-08-01 12:06:36 +0530

    v1

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Your test paypal sanbox url, Replace it with live url after successful testing.
+DEFINE('PAYPAL_URL', 'https://www.sandbox.paypal.com/cgi-bin/webscr'); 
+
+// Set paypal marchent id.
+DEFINE('PAYPAL_ID', 'rohit.php5-facilitator@gmail.com'); 
+
+// Define your base currency
+DEFINE('CURRENCY', 'USD')
+
+
+?>
